02e84f8be2aa5853e20eb65aae8b754cd114e1b2
kumawatiitk <vishalkumawat01@gmail.com>
2026-07-25 22:37:06 -0700

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-2

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-2.cs
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public TreeNode BuildTree(int[] preorder, int[] inorder) {
+        return BuildTree(preorder, 0, preorder.Length - 1, inorder, 0, inorder.Length - 1);
+    }
+
+    public TreeNode BuildTree(int[] preorder, int ps, int pe, int[] inorder, int iss, int ie) 
+    {
+        if(ps > pe) return null;
+        var newNode = new TreeNode(preorder[ps]);
+        
+        if(ps == pe) return newNode;
+
+        var index = binarySearch(inorder, preorder[ps], iss, ie);
+        var totalElementInLeftTree = index - iss;
+
+        var leftNode = BuildTree(preorder, ps + 1, ps + totalElementInLeftTree, inorder, iss, index - 1);
+        var rightNode = BuildTree(preorder, ps + totalElementInLeftTree + 1, pe, inorder, index + 1, ie);
+
+        newNode.left = leftNode;
+        newNode.right = rightNode;
+
+        return newNode;
+    }
+
+    public int binarySearch(int[] inorder, int key, int s, int e) {
+        for(int i = s; i <= e; i++) {
+            if(key == inorder[i]) return i;
+        }
+
+        return -1;
+
+    }
+
+}
