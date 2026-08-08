f4725e38aa9e35d223df46b92c94c74e1cf296e4
kumawatiitk <vishalkumawat01@gmail.com>
2026-08-08 10:54:02 -0700

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.cs
@@ -0,0 +1,80 @@
+public class PrefixTree {
+
+    TreeNode root;
+
+    public PrefixTree() {
+        root = new TreeNode(' ');
+    }
+    
+    public void Insert(string word) {
+        Insert(word, 0, root);
+    }
+
+    public void Insert(String word, int index, TreeNode node) {
+
+        if(index == word.Length) {
+            node.SetWord();
+            return;
+        }
+
+        node.nodes[word[index] - 'a'] =    node.nodes[word[index] - 'a']  ?? new TreeNode(word[index]); 
+        Insert(word, index + 1,  node.nodes[word[index] - 'a'] );
+    }
+    
+    public bool Search(string word) {
+        return Search(word, 0, root);
+    }
+
+    public bool Search(String word, int index, TreeNode node) {
+
+        if(index >= word.Length) {
+            return false;
+        }
+
+         if(node.nodes[word[index] - 'a'] == null) {
+           return false;
+        }
+
+        if(index == word.Length - 1) {
+           return  node.nodes[word[index] - 'a'].GetWord();
+        } 
+
+        return Search(word, index + 1,  node.nodes[word[index] - 'a'] );
+    }
+    
+    public bool StartsWith(string prefix) {
+        return StartsWith(prefix, 0, root);
+    }
+
+     public bool StartsWith(string word, int index, TreeNode node) {
+        if(index >= word.Length) {
+            return true;
+        }
+
+        if(node.nodes[word[index] - 'a'] == null) {
+           return false;     
+        }
+
+        return StartsWith(word, index + 1,  node.nodes[word[index] - 'a'] );
+     }
+}
+
+public class TreeNode {
+
+    public TreeNode[] nodes;
+    char c;
+    bool isWord;
+    public TreeNode(char s, bool word = false) {
+        c = s;
+        nodes = new TreeNode[26];
+        isWord = word;
+    }
+
+    public void SetWord() {
+        isWord = true;
+    }
+
+    public bool GetWord() {
+        return isWord;
+    }
+}
